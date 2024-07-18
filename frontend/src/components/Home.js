a4cf13ae7b993db7dff90e6e7e0bb23bfceef9ae
Hem Kishore Pradhan <hemkishoresoa@gmail.com>
2024-07-19 04:40:21 +0530

    added

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,27 @@
+  import React, { useContext, useEffect, useState } from 'react'
+//   import styled from 'styled-components'
+//   import { useLocation, useNavigate } from 'react-router-dom'
+//   import Img from '../image/background.jpg'
+//  import { AuthContext } from '../AuthContext';
+ import '../cssfile/Home.css';
+
+ function Home() {
+
+   return (
+  
+       
+         <div className='bd'>
+           <p>
+               Phising website Detection
+           </p>
+           
+         </div>
+        
+   )
+ }
+
+ export default Home
+
+
+
+ 
